98987e87194a769652ddc746091fa8d215d70c88
Abraham Polk <abe@Abrahams-MacBook-Air.local>
2020-05-27 20:46:30 -0400

    More CSS again

--- views/Show.jsx
+++ views/Show.jsx
@@ -31,23 +31,27 @@ class Show extends React.Component {
         const postedDate = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'long', day: 'numeric' }).format(job.createdAt);
         return (
             <Default authType={authType} additionalCss="Show.css">
-                <a href="/">View all jobs</a>
+                <div id="user-options-bar">
+                    <a class="user-option" href="/">View all jobs</a>
+                </div>
                     <div class="shadow-box">
                         <h1>{job.title}</h1>
                         <h3 class="company-name">{job.company}</h3>
                         <h3 class="location">{job.location}</h3>
                         <h5>Posted: {postedDate}</h5>
                         <h4>Job Description:</h4>
-                        <p>{job.description}</p>
-                        <h4>Skills required</h4>
-                        <ul>
-                            {job.skills.map((skill, index) => {
-                                return (
-                                    <li key={index}>{skill}</li>
-                                )
-                                }
-                            )}
-                        </ul>
+                        <div id="description-skills-container">
+                            <p>{job.description}</p>
+                            <h4>Skills required:</h4>
+                            <ul id="skills-list">
+                                {job.skills.map((skill, index) => {
+                                    return (
+                                        <li key={index}>{skill}</li>
+                                    )
+                                    }
+                                )}
+                            </ul>
+                        </div>
                     </div>
                     <ApplyButton id={job._id} canApply={canApply} />
                     <EditDelete isOwner={isOwner} id={job._id} />
